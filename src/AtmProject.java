89b8217cf7d085d02d81381384941c8582fa6155
adem <adem@gmail.com>
2023-04-27 21:54:46 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,5 +2,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu Bir Atm Projesidir");
         // versiyon1
+        System.out.println("Java is easy");
+        // versiyon2
     }
 }
